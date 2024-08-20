e7079f618dc4f12b4e3f2acef3da4a2571999697
roiLeo <medina.leo42@gmail.com>
2024-08-20 14:02:58 +0200

    ✨ google & github sign in

--- components/auth/Login.vue
+++ components/auth/Login.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="space-y-4">
     <div class="flex gap-4">
-      <!-- <UButton
-        v-for="(provider, key) in providers"
-        :key="key"
-        :loading="provider.loading"
-        :icon="provider.icon"
-        :color="provider.color"
-        :label="provider.name"
+      <UButton
+        v-if="config.public.googleAuth"
+        to="/auth/google"
+        label="Sign-in with Google"
+        icon="i-simple-icons-google"
+        color="white"
         size="lg"
-        class="w-full flex items-center justify-center"
-        @click="loginVia(key as string)"
-      /> -->
+        external
+        block
+      />
+      <UButton
+        v-if="config.public.githubAuth"
+        to="/auth/github"
+        label="Sign-in with GitHub"
+        icon="i-simple-icons-github"
+        color="black"
+        size="lg"
+        external
+        block
+      />
     </div>
 
-    <UDivider label="OR" />
+    <!-- <UDivider label="OR" /> -->
 
-    <UForm ref="form" :state="state" class="space-y-4">
+    <!-- <UForm ref="form" :state="state" class="space-y-4">
       <UFormGroup label="Email" name="email" required>
         <UInput
           v-model="state.email"
@@ -41,14 +50,14 @@
       </UTooltip>
 
       <div class="flex items-center justify-end space-x-4">
-        <UButton type="submit" label="Login" />
+        <UButton type="submit" label="Sign in" block />
       </div>
-    </UForm>
+    </UForm> -->
 
-    <div class="text-sm">
+    <!-- <div class="text-sm">
       Don't have an account yet?
       <NuxtLink class="text-sm" to="/auth/register">Sign up now</NuxtLink>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -57,13 +66,6 @@ const config = useRuntimeConfig()
 // const router = useRouter()
 // const auth = useAuthStore()
 // const form = ref()
-
-type Provider = {
-  name: string
-  icon: string
-  color: string
-  loading?: boolean
-}
 
 const state = reactive({
   email: "",
@@ -87,49 +89,4 @@ const state = reactive({
 //     }
 //   }
 // })
-
-const providers = ref<{ [key: string]: Provider }>(config.public.providers)
-
-// async function handleMessage(event: { data: any }): Promise<void> {
-//   const provider = event.data.provider as string
-
-//   if (Object.keys(providers.value).includes(provider) && event.data.token) {
-//     providers.value[provider].loading = false
-//     auth.token = event.data.token
-
-//     await auth.fetchUser()
-//     await router.push("/")
-//   } else if (event.data.message) {
-//     useToast().add({
-//       icon: "i-heroicons-exclamation-circle-solid",
-//       color: "red",
-//       title: event.data.message,
-//     })
-//   }
-// }
-
-// function loginVia(provider: string): void {
-//   providers.value[provider].loading = true
-
-//   const width = 640
-//   const height = 660
-//   const left = window.screen.width / 2 - width / 2
-//   const top = window.screen.height / 2 - height / 2
-
-//   const popup = window.open(
-//     `${config.public.apiBase}${config.public.apiPrefix}/login/${provider}/redirect`,
-//     "Sign In",
-//     `toolbar=no, location=no, directories=no, status=no, menubar=no, scollbars=no, resizable=no, copyhistory=no, width=${width},height=${height},top=${top},left=${left}`
-//   )
-
-//   const interval = setInterval(() => {
-//     if (!popup || popup.closed) {
-//       clearInterval(interval)
-//       providers.value[provider].loading = false
-//     }
-//   }, 500)
-// }
-
-// onMounted(() => window.addEventListener("message", handleMessage))
-// onBeforeUnmount(() => window.removeEventListener("message", handleMessage))
 </script>
